f202cd3e5322a918d877df3b762627a38afba20a
sophialadwiniec <slladwiniec@dons.usfca.edu>
2021-02-25 10:00:40 -0800

    pushing code to github

--- sketch.js
+++ sketch.js
@@ -1,14 +1,6 @@
 /***********************************************************************************
 	SimpleStateMachine - TEMPLATE
-	by Scott Kildall
-
-	Template:
-
-	(1) Add your own PNG files in the assets folder. Make sure they match the names ***exactly*** of the existing PNGs.
-	(2) Add custom drawing code to drawOne(), drawTwo(), drawThree(), drawFour(), drawFive()
-	(3) You can add your own interfaces - keys, mouse events, etc in the Interfaces section
-
-	Also start your localhost before running this, otherwise no PNGs will display
+	by Sophia Ladwiniec
 
 ------------------------------------------------------------------------------------
 	The way it works — you don't need to know this for the template use
@@ -35,8 +27,8 @@
 var images = [];
 // Array of functions
 var functions = []; 
-// Array of instructions 
-var instructions = []; 
+// // Array of instructions 
+// var instructions = []; 
 // Array of colors 
 var colors = []; 
 
@@ -53,52 +45,53 @@ var gTextOffset = 20;
 
 // load all images into an array
 function preload() {
-  images[0] = loadImage('assets/angry.png');
-  images[1] = loadImage('assets/calm.png');
-  images[2] = loadImage('assets/happy.png');
-  images[3] = loadImage('assets/sad.png');
-  images[4] = loadImage('assets/scared.png');
-  images[5] = loadImage('assets/splash.png');
+  images[0] = loadImage('assets/House front.png');
+  images[1] = loadImage('assets/Foyer.png');
+  images[2] = loadImage('assets/Dining room.png');
+  images[3] = loadImage('assets/Kitchen.png');
+  images[4] = loadImage('assets/stairs.png');
+  images[5] = loadImage('assets/Upstairs room.png');
+  images[6] = loadImage('assets/roof.png');
 }
 
 function loadFunctionsArray() {
-  functions[0] = drawOne; 
-  functions[1] = drawTwo; 
-  functions[2] = drawThree; 
-  functions[3] = drawFour; 
-  functions[4] = drawFive; 
-  functions[5] = drawSplash; 
-  functions[6] = drawInstructions; 
+  functions[0] = houseFront; 
+  functions[1] = foyer; 
+  functions[2] = dinningRoom; 
+  functions[3] = kitchen; 
+  functions[4] = stairs; 
+  functions[5] = upstairs; 
+  functions[6] = roof; 
 }
 
-function loadInstructionsArray() {
-  instructions[0] = "Press 1 for Angry!"; 
-  instructions[1] = "Press 2 for Calm"; 
-  instructions[2] = "Press 3 for Happy :)"; 
-  instructions[3] = "Press 4 for Sad :(";
-  instructions[4] = "Press 5 for Scared"
-  instructions[5] = "Press s for a splash of bubbles!"; 
-  instructions[6] = "Press i for instructions or click on the bubble screen"; 
-}
+// function loadInstructionsArray() {
+//   instructions[0] = "Press 1 for Angry!"; 
+//   instructions[1] = "Press 2 for Calm"; 
+//   instructions[2] = "Press 3 for Happy :)"; 
+//   instructions[3] = "Press 4 for Sad :(";
+//   instructions[4] = "Press 5 for Scared"
+//   instructions[5] = "Press s for a splash of bubbles!"; 
+//   instructions[6] = "Press i for instructions or click on the bubble screen"; 
+// }
 
 
-function loadColors() {
-  colors[0] = "#FF0000"; 
-  colors[1] = "#008000"; 
-  colors[2] = "#FFFF00"; 
-  colors[3] = "#0000FF";
-  colors[4] = "#800080"; 
-  colors[5] = "#4169e1"; 
-  colors[6] = "#ae0700"; 
-}
+// function loadColors() {
+//   colors[0] = "#FF0000"; 
+//   colors[1] = "#008000"; 
+//   colors[2] = "#FFFF00"; 
+//   colors[3] = "#0000FF";
+//   colors[4] = "#800080"; 
+//   colors[5] = "#4169e1"; 
+//   colors[6] = "#ae0700"; 
+// }
 
 // Center drawing, drawFunction will be one for default
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 600);
   // loading all arrays 
   loadFunctionsArray(); 
-  loadInstructionsArray(); 
-  loadColors(); 
+  // loadInstructionsArray(); 
+  // loadColors(); 
 
   midX = width/2;
   startY = 60;
@@ -109,7 +102,7 @@ function setup() {
   textSize(24);
 
   // set to one for startup
-  drawFunction = functions[5];
+  drawFunction = functions[0];
 }
 
 // Very simple, sets the background color and calls your state machine function
@@ -123,87 +116,124 @@ function draw() {
 //========= TEMPLATE: modify these functions, INSIDE the function blocks only =========
 
 //-- drawOne() will draw the image at index 0 from the array
-drawOne = function() {
+houseFront = function() {
    image(images[0],width/2, height/2);
 
-   fill(colors[0]);
-   text("Angry", width/2, height - gTextOffset);
+   fill(255); 
+   text("Front of House", width/2, height - gTextOffset);
 }
 
 //-- drawTwo() will draw the image at index 1 from the array
-drawTwo = function() {
+foyer = function() {
    image(images[1],width/2, height/2);
 
-   fill(colors[1]);
-   text("Calm", width/2, height - gTextOffset);
+   fill(255); 
+   text("Foyer", width/2, height - gTextOffset);
 }
 
 //-- drawOne() will draw the image at index 2 from the array
-drawThree = function() {
+dinningRoom = function() {
    image(images[2],width/2, height/2);
 
-   fill(colors[2]);
-   text("Happy", width/2, height - gTextOffset);
+   fill(255);
+   text("Dining Room", width/2, height - gTextOffset);
 }
 
 //-- drawOne() will draw the image at index 3 from the array
-drawFour = function() {
+kitchen = function() {
    image(images[3],width/2, height/2);
 
-   fill(colors[3]);
-   text("Sad", width/2, height - gTextOffset);
+   fill(255);
+   text("Kitchen", width/2, height - gTextOffset);
 }
 
 //-- drawOne() will draw the image at index 4 from the array
-drawFive = function() {
+stairs = function() {
    image(images[4],width/2, height/2);
 
-   fill(colors[4]);
-   text("Scared", width/2, height - gTextOffset);
+   fill(255);
+   text("Stairs", width/2, height - gTextOffset);
 }
 
-drawSplash = function() {
+upstairs = function() {
    image(images[5],width/2, height/2);
+   fill(255);
+   text("Upstairs Room", width/2, height - gTextOffset);
    
 }
 
-drawInstructions = function() {
-  for( let i = 0; i < instructions.length; i++ ) {
-    fill(colors[i]); 
-    text( instructions[i], midX, startY + (i * lineHeight) )
-  }
+roof = function() {
+   image(images[6],width/2, height/2);
+   fill(255);
+   text("Roof", width/2, height - gTextOffset);
+   
 }
+
 
 //========= TEMPLATE: add or change interface functions, as you like =========
 
 // Change the drawFunction variable based on your interaction
 function keyTyped() {
-  if( key === '1' ) {
-  	drawFunction = functions[0];
+  if (drawFunction === houseFront){
+    if(key === 'e'){
+      drawFunction = foyer; 
+    }
   }
-  else if( key === '2' ) {
-  	drawFunction = functions[1];
+  if (drawFunction === foyer){
+    if(key === 'o'){
+      drawFunction = houseFront; 
+    }
+    if(key === 'd'){
+      drawFunction = dinningRoom; 
+    }
+    if(key === 's'){
+      drawFunction = stairs; 
+    }
   }
-  else if( key === '3' ) {
-  	drawFunction = functions[2];
+
+  if (drawFunction === dinningRoom){
+    if(key === 'f'){
+      drawFunction = foyer; 
+    }
+    if(key === 'k'){
+      drawFunction = kitchen; 
+    }
   }
-  else if( key === '4' ) {
-  	drawFunction = functions[3];
+
+  if (drawFunction === kitchen){
+    if(key === 's'){
+      drawFunction = stairs; 
+    }
+    if(key === 'd'){
+      drawFunction = dinningRoom; 
+    }
   }
-  else if( key === '5' ) {
-  	drawFunction = functions[4];
+
+  if (drawFunction === stairs){
+    if(key === 'f'){
+      drawFunction = foyer; 
+    }
+    if(key === 'k'){
+      drawFunction = kitchen; 
+    }
+    if(key === 'u'){
+      drawFunction = upstairs; 
+    }
   }
-  else if( key == 's') {
-    drawFunction = functions[5]; 
+
+  if (drawFunction === upstairs){
+    if(key === 's'){
+      drawFunction = stairs; 
+    }
+    if(key === 'r'){
+      drawFunction = roof; 
+    }
   }
-  else if( key == 'i') {
-    drawFunction = functions[6]; 
+
+  if (drawFunction === roof){
+    if(key === 'u'){
+      drawFunction = upstairs; 
+    }
   }
 }
 
-function mousePressed() {
-  // only change state if we are in splash screen
-  if( drawFunction === drawSplash ) {
-    drawFunction = functions[6];
-  }
-}
